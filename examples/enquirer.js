3d1cf4c5f11da3fc82b0cb11fa58c425ccd8337a
doowb <brian.woodward@gmail.com>
2016-10-20 18:04:55 -0400

    update example to show setting default value to false

--- examples/enquirer.js
+++ examples/enquirer.js
@@ -4,7 +4,7 @@ var enquirer = new Enquirer();
 enquirer.register('confirm', require('..'));
 
 enquirer.question('chocolate', 'Like chocolate?', {type: 'confirm'});
-enquirer.question('vanilla', 'Like vanilla?', {type: 'confirm'});
+enquirer.question('vanilla', 'Like vanilla?', {type: 'confirm', default: false});
 
 enquirer.ask(['chocolate', 'vanilla'])
   .then(function(answers) {
